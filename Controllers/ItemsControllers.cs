7d9389eed2553a66daa305c07e7e603c4a31d869
Ian Goodrich <iangoodrich@hotmail.com>
2018-02-08 06:46:18 -0800

    add Category class and functionality. Still buggie.

--- Controllers/ItemsControllers.cs
+++ Controllers/ItemsControllers.cs
@@ -7,25 +7,35 @@ namespace ToDoList.Controllers
     public class ItemsController : Controller
     {
 
+      [HttpGet("/categories/{categoryId}/items/new")]
+      public ActionResult CreateForm(int categoryId)
+      {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Category foundCategory = Category.Find(categoryId);
+        List<Item> allItems = foundCategory.GetItems();
+        model.Add("items", allItems);
+        return View(model);
+      }
+
       [HttpGet("/items")]
       public ActionResult Index()
       {
           List<Item> allItems = Item.GetAll();
           return View(allItems);
       }
+      //
+      // [HttpGet("/items/new")]
+      // public ActionResult CreateForm()
+      // {
+      //     return View();
+      // }
 
-      [HttpGet("/items/new")]
-      public ActionResult CreateForm()
-      {
-          return View();
-      }
-
-      [HttpPost("/items/create")]
-      public ActionResult CreateItem()
-      {
-          Item newItem = new Item(Request.Form["new-item"]);
-          return View(newItem);
-      }
+      // [HttpPost("/items/create")]
+      // public ActionResult CreateItem()
+      // {
+      //     Item newItem = new Item(Request.Form["new-item"]);
+      //     return View(newItem);
+      // }
 
       [HttpPost("/items/delete")]
       public ActionResult DeleteAll()
@@ -35,10 +45,14 @@ namespace ToDoList.Controllers
       }
 
       [HttpGet("/items/{id}")]
-      public ActionResult Detail(int id)
-      {
-          Item item = Item.Find(id);
-          return View(item);
-      }
+        public ActionResult Details(int id)
+        {
+            Item item = Item.Find(id);
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Category category = Category.Find(item.GetId());
+            model.Add("item", item);
+            model.Add("category", category);
+            return View(item);
+        }
     }
 }
